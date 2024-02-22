6c224af9dacbd769610f938772368fec115d5bbf
himbarbuana <143574816+himbarbuana@users.noreply.github.com>
2024-02-22 23:00:58 +0700

    Merge remote-tracking branch 'origin/main'

--- golang_programmer_zaman_now/14_operasi_perbandingan.go
+++ golang_programmer_zaman_now/14_operasi_perbandingan.go
@@ -24,4 +24,14 @@ func main() {
 	var name2 = "Himbar"
 	var nameResult bool = name1 == name2
 	fmt.Println(nameResult)
+
+	var b = "B"
+	var a = "A"
+	var ba bool = b > a
+	fmt.Println(ba)
+
+	var nilai1 = 100
+	var nilai2 = 75
+	var nilai bool = nilai1 < nilai2
+	fmt.Println(nilai)
 }
